569666f832129f77bee333aa5b784c680f63bf5f
Jasmine <jasmine.lin@oocl.com>
2025-09-05 16:30:27 +0800

    feat: implemented turn-left test

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,9 +1,13 @@
 package com.afs.tdd;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MarsRover {
 
 
     private Location location;
+    private List<Direction> directions= Arrays.asList(Direction.N,Direction.E,Direction.S,Direction.W);
 
     public MarsRover(Location location) {
         this.location = location;
@@ -18,13 +22,26 @@ public class MarsRover {
     }
 
     public void executeCommand(Command command) {
-        if (command.equals(Command.M)){
-            switch(location.getDirection()){
-                case E:{location.setLocationX(location.getLocationX()+1);break;}
-                case N:{location.setLocationY(location.getLocationY()+1);break;}
-                case S:{location.setLocationY(location.getLocationY()-1);break;}
-                case W:{location.setLocationX(location.getLocationX()-1);break;}
+        switch(command){
+            case M:{
+                switch(location.getDirection()){
+                    case E:{location.setLocationX(location.getLocationX()+1);break;}
+                    case N:{location.setLocationY(location.getLocationY()+1);break;}
+                    case S:{location.setLocationY(location.getLocationY()-1);break;}
+                    case W:{location.setLocationX(location.getLocationX()-1);break;}
+                }
+                break;
             }
+            case L:{
+                location.setDirection(
+                        directions.get(
+                                (directions.size()
+                                        + directions.indexOf(location.getDirection())
+                                        - 1) % directions.size()));
+
+                break;
+            }
+
         }
     }
 }

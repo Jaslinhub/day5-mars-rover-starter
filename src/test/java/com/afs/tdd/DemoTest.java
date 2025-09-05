4e9a0a131976d9dc232b34c0edfd40c9c14bfe4d
Jasmine <jasmine.lin@oocl.com>
2025-09-05 18:37:39 +0800

    test: add all origin direction in east test

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -42,6 +42,18 @@ class DemoTest {
         assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
     }
     @Test
+    void should_move_when_executeCommand_given_M_in_east(){
+        Location location=new Location(0,0,Direction.E);
+        MarsRover marsRover=new MarsRover(location);
+
+        marsRover.executeCommand(Command.M);
+
+        Location currentLocation =new Location(1,0,Direction.E);
+        assertEquals(currentLocation.getLocationY(),marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(),marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
+    }
+    @Test
     void should_turn_left_when_executeCommand_given_L_in_north(){
         Location location=new Location(0,0,Direction.N);
         MarsRover marsRover=new MarsRover(location);
@@ -78,6 +90,18 @@ class DemoTest {
         assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
     }
     @Test
+    void should_turn_left_when_executeCommand_given_L_in_east(){
+        Location location=new Location(0,0,Direction.E);
+        MarsRover marsRover=new MarsRover(location);
+
+        marsRover.executeCommand(Command.L);
+
+        Location currentLocation =new Location(0,0,Direction.N);
+        assertEquals(currentLocation.getLocationY(),marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(),marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
+    }
+    @Test
     void should_turn_right_when_executeCommand_given_R_in_north(){
         Location location=new Location(0,0,Direction.N);
         MarsRover marsRover=new MarsRover(location);
@@ -109,6 +133,18 @@ class DemoTest {
         marsRover.executeCommand(Command.R);
 
         Location currentLocation =new Location(0,0,Direction.N);
+        assertEquals(currentLocation.getLocationY(),marsRover.getLocation().getLocationY());
+        assertEquals(currentLocation.getLocationX(),marsRover.getLocation().getLocationX());
+        assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
+    }
+    @Test
+    void should_turn_right_when_executeCommand_given_R_in_east(){
+        Location location=new Location(0,0,Direction.E);
+        MarsRover marsRover=new MarsRover(location);
+
+        marsRover.executeCommand(Command.R);
+
+        Location currentLocation =new Location(0,0,Direction.S);
         assertEquals(currentLocation.getLocationY(),marsRover.getLocation().getLocationY());
         assertEquals(currentLocation.getLocationX(),marsRover.getLocation().getLocationX());
         assertEquals(currentLocation.getDirection(),marsRover.getLocation().getDirection());
